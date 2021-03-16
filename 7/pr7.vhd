9dcd4c32beb841d4298bb40c10df247cc1b821f6
Quintanaaalberto <79100197+Quintanaaalberto@users.noreply.github.com>
2021-03-16 18:36:11 +0100

    Update pr7.vhd

--- 7/pr7.vhd
+++ 7/pr7.vhd
@@ -12,8 +12,34 @@ entity  is
 	) ;
 end ;
 
-architecture _arc of  is
+architecture pr7_arc of  pr7 is
 
+	COMPONENT alu
+			PORT(
+				A_in, B_in 		:in std_logic_vector(4 downto 0);
+				sel_in			:in std_logic_vector(2 downto 0);
+				S_out			:out std_logic_vector(4 downto 0);
+				OV_out			:out std_logic;
+				disp1, disp2 	:out std_logic_vector(6 downto 0)
+				);
+		END COMPONENT;
+	COMPONENT BinA7Seg_Dec
+			PORT(
+				N 			: in std_logic_vector (5 downto 0);
+				Display1 	: out std_logic_vector (6 downto 0);
+				Display2 	: out std_logic_vector (6 downto 0)
+				);
+		END COMPONENT;
 begin
 
-end  _arc;
+	ialu : alu
+			PORT MAP(
+				A_in => a, B_in => b, sel_in => sel,
+				S_out => S, OV_out => ov
+				);
+	idisplay : BinA7Seg_Dec
+			PORT MAP(
+				N =>  S, Display1 => disp1, Display2 => disp2
+				);
+
+end  pr7_arc;
